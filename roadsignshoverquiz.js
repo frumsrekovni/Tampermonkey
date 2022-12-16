2a0f250bf245518b22f9d57cc4e6b4e615014a46
frumsrekovni <joshua.backstrom@gmail.com>
2022-12-16 23:55:34 +0100

    -Added the complete file

--- roadsignshoverquiz.js
+++ roadsignshoverquiz.js
@@ -1,16 +1,17 @@
-
+// ==UserScript==
+// @name         Road Signs Hover Quiz
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Makes Transportstyrelsens website function essentially like flash card practice
+// @author       You
+// @match        https://www.transportstyrelsen.se/sv/vagtrafik/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=transportstyrelsen.se
+// @grant        none
+// ==/UserScript==
 (function() {
-
     'use strict';
-
     document.body.insertAdjacentHTML("beforeend",`<style>
-
     .roadsignlink span {opacity:0%}
-
     .roadsignlink:hover span {opacity:100%}
-
     </style>`);
-
-    // Your code here...
-
 })();
