8d28816f3ef80154ad503802a3f56f17541b62ba
frumsrekovni <joshua.backstrom@gmail.com>
2022-12-16 23:53:53 +0100

    -Added a script that makes Transportstyrelsen road signs function like flash card practice

---
+++ roadsignshoverquiz.js
@@ -0,0 +1,16 @@
+
+(function() {
+
+    'use strict';
+
+    document.body.insertAdjacentHTML("beforeend",`<style>
+
+    .roadsignlink span {opacity:0%}
+
+    .roadsignlink:hover span {opacity:100%}
+
+    </style>`);
+
+    // Your code here...
+
+})();
